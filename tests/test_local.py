9da8802dcadea81a35488d64116a47dc4c301bd5
Joshua Cherry <joshuaRcherry@gmail.com>
2017-04-07 16:06:52 -0400

    updated tests.

--- tests/test_local.py
+++ tests/test_local.py
@@ -2,19 +2,33 @@
 import yaml
 # Requires: sudo apt-get install python3-pip -y && sudo pip3 install pyyaml
 from subprocess import check_call, CalledProcessError
-import sys, os
+from collections import deque
+import sys, os, subprocess
 
 travisFile = os.path.join(os.path.dirname(__file__), '../.travis.yml')
 
+def appendToQueue(cmd):
+    cmd_queue.append('printf "\\n${green}Running Command: '+cmd+'${neutral}\\n"')
+    cmd_queue.append(cmd)
+    #cmd_queue.append('printf "\\n${yellow}The command \\"'+cmd+'\\" exited with $?.${neutral}\\n"')
+
 with open(travisFile, 'r') as stream:
+    cmd_queue = deque()
+    cmd_queue.append('set -e')
+    cmd_queue.append("red='\033[0;31m'")
+    cmd_queue.append("green='\033[0;32m'")
+    cmd_queue.append("yellow='\033[0;33m'")
+    cmd_queue.append("neutral='\033[0m'")
     try:
         data = yaml.load(stream)
         for cmd in data['script']:
-            command = check_call(cmd, shell=True)
-            print ("\033[0;33m\'%s'\033[0m\nTest: \033[0;32mpass\033[0m\nreturned: %s\n" % (cmd,str(command)))
+            appendToQueue(cmd)
         for cmd in data['after_success']:
-            command = check_call(cmd, shell=True)
-            print ("\033[0;33m\'%s'\033[0m\nTest: \033[0;32mpass\033[0m\nreturned: %s\n" % (cmd,str(command)))
+            appendToQueue(cmd)
+        bash_script = "; ".join(cmd_queue)
+        #print('\n'+bash_script+'\n\n') #debugging
+        command = check_call(bash_script, shell=True)
+        print ("\nTest: \033[0;32mpass\033[0m\nreturned: %s\n" % (str(command)))
 
 
     except yaml.YAMLError as exc:
